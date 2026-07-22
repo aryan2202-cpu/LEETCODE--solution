0287db0dc09b06d74545dad29f0b1dea0e3f0451
Aryan Verma <138443182+aryan2202-cpu@users.noreply.github.com>
2026-07-22 05:43:05 +0000

    Sync LeetCode submission Runtime - 3 ms (87.20%), Memory - 15.4 MB (5.30%)

---
+++ leetcode-problems/0147-insertion-sort-list/solution.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head){
+         vector<int> arr;
+        ListNode* temp = head;
+        if(temp==NULL){
+          return nullptr;
+        }
+       while(temp!=NULL){
+        arr.push_back(temp->val);
+        temp = temp->next;
+       }
+       int n = arr.size();
+       sort(arr.begin(),arr.end());
+        ListNode* heads = new ListNode(arr[0]);
+        ListNode* mover = heads;
+        for(int i =1;i<arr.size();i++){
+            ListNode* temper = new ListNode(arr[i]);
+            mover->next = temper;
+            mover = temper;
+        }
+        return heads;
+    }
+};
+    
