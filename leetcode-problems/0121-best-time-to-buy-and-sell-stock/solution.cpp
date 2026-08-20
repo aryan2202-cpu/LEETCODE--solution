2084df5adf084c5012061b4ad983b81bf0096426
Aryan Verma <138443182+aryan2202-cpu@users.noreply.github.com>
2026-08-20 16:16:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 97.3 MB (87.97%)

---
+++ leetcode-problems/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int profit = 0;
+        int min = prices[0];
+        for(int i = 0;i<n;i++){
+            if(prices[i] < min){
+                min = prices[i];
+            } else if(prices[i] - min > profit){
+                profit = prices[i] - min;
+            }
+        }
+        return profit;
+    }
+};
