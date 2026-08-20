a9ef71351c33b82911ba55c4763fa26f894e5ccd
Aryan Verma <138443182+aryan2202-cpu@users.noreply.github.com>
2026-08-20 12:34:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.7 MB (42.00%)

---
+++ leetcode-problems/0543-diameter-of-binary-tree/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxi = 0;
+
+    int Max(TreeNode* root){
+        if(root == NULL) return 0;
+        int lh = Max(root->left);
+        int rh = Max(root->right);
+        maxi = max(maxi, lh+rh);
+        return max(lh,rh) + 1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        Max(root);
+        return maxi;
+    }
+};
